4820336ddf36044003e7038148350287859d5587
Grzegorz Klimek <kfiku.com@gmail.com>
2023-12-03 18:54:50 +0100

    day 3 part 2

--- 2023/03/index.ts
+++ 2023/03/index.ts
@@ -9,76 +9,100 @@ async function run() {
   for await (const line of readline(f)) {
     const textLine = new TextDecoder().decode(line);
     if (lines.length < 3) {
-      lines.push(`.${textLine}.`)
+      lines.push(`.${textLine}.`);
     } else {
-      lines.shift()
-      lines.push(`.${textLine}.`)
+      lines.shift();
+      lines.push(`.${textLine}.`);
       const nr = processLine(lines);
       numbers.push(nr);
     }
   }
 
-  lines.shift()
-  lines.push('')
+  lines.shift();
+  lines.push("");
   const nr = processLine(lines);
   numbers.push(nr);
 
-  console.log(numbers);
-  console.log(4361, sum(numbers));
+  // console.log(numbers);
+  const result = sum(numbers);
+  console.log(467835 === result, 467835, result);
 
   f.close();
 }
 
 function processLine(lines: string[]) {
-  const properNumbers = [0]
-  let current = ""
-  let border = ""
+  const properNumbers = [0];
   for (let i = 0; i < lines[1].length; i++) {
     const d = lines[1][i];
-    if (Number.isInteger(+d)) {
-      if (border === "" && i > 0) {
-        border += lines[1][i - 1]
-      }
+    if (d === "*") {
+      const n = findNumbers(lines, i);
 
-      current += d
-    } else if(current) {
-      const start = i - current.length - 1
-      const end = i + 1
-      border += d
-      border += lines[0].substring(start, end)
-      border += lines[2].substring(start, end)
-
-      if (border.replace(/[\d. ]/g, '') !== "") {
-        // console.log(current, 'OK');
-        properNumbers.push(+current)
-      } else {
-        console.log(current);
-      }
-
-      current = border = ""
+      properNumbers.push(n);
     }
-
   }
-  // const properNumbers = numbersInLine.map(n => {
-  //   const pos = lines[1].indexOf(n);
-  //   const start = !pos ? pos : pos - 1;
-  //   const end = pos + n.length
-  //   const border = ""
 
-  //   if (border.replace(/[\d. ]/g, '') === "") {
-  //     console.log(n, "wrong", border, start, end);
+  return sum(properNumbers);
+}
 
-  //     return 0
-  //   }
+function findNumbers(lines: string[], starPos: number) {
+  let numbers: number[] = [];
 
-  //   console.log(n, 'OK');
+  numbers.push(...findInLine(lines[0], starPos));
+  numbers.push(...findInLine(lines[1], starPos));
+  numbers.push(...findInLine(lines[2], starPos));
+  numbers = numbers.filter(Boolean);
 
-  //   return +n
-  // });
+  if (numbers.some((n) => n == 67)) {
+    console.log(numbers);
+  }
+  if (numbers.length === 2) {
+    // console.log(numbers);
+    return numbers[0] * numbers[1];
+  }
 
-  // console.log(properNumbers);
+  return 0;
+}
 
-  return sum(properNumbers)
+function findInLine(line: string, pos: number) {
+  const numbers: number[] = [];
+  const point = line[pos];
+  const left = line[pos - 1];
+  const right = line[pos + 1];
+
+  if (Number.isInteger(+point)) {
+    numbers.push(findInNrInLine(line, pos));
+  } else {
+    if (Number.isInteger(+left)) {
+      numbers.push(findInNrInLine(line, pos - 1));
+    }
+    if (Number.isInteger(+right)) {
+      numbers.push(findInNrInLine(line, pos + 1));
+    }
+  }
+
+  return numbers;
+}
+
+function findInNrInLine(line: string, pos: number, log = 0) {
+  let i = pos;
+  let d = line[i];
+  let num = "";
+  log && console.log("R", d, i);
+
+  while (Number.isInteger(+d)) {
+    i--;
+    d = line[i];
+  }
+
+  i++;
+  d = line[i];
+  while (Number.isInteger(+d)) {
+    i++;
+    num += d;
+    d = line[i];
+  }
+
+  return +num || 0;
 }
 
 function sum(numbers: number[]) {
