2cf5d0ee7d6904fc2bf70c01406c190552f0e9a4
Grzegorz Klimek <kfiku.com@gmail.com>
2023-12-06 09:51:13 +0100

    day 6

---
+++ 2023/06/index.ts
@@ -0,0 +1,69 @@
+import { lineByLine, multiply, printResults } from "../utils/utils.ts";
+
+const part = +Deno.args[0] || 1;
+
+async function run() {
+  if (part === 1) {
+    await lineByLine("./input.txt", part2);
+    const result = process();
+
+    printResults(114400, [result]);
+  } else {
+    await lineByLine("./input.txt", part1);
+    const result = process();
+
+    printResults(21039729, [result]);
+  }
+}
+
+let times: number[] = [];
+let records: number[] = [];
+
+function part1(line: string) {
+  const [key, value] = line.split(":");
+
+  const values = [+(value.replace(/ +/g, ""))];
+
+  if (key === "Time") {
+    times = values;
+  } else {
+    records = values;
+  }
+}
+
+function part2(line: string) {
+  const [key, value] = line.split(":");
+
+  const values = value.trim().split(/ +/).map((v) => +(v.trim()));
+
+  if (key === "Time") {
+    times = values;
+  } else {
+    records = values;
+  }
+}
+
+function process() {
+  const wins: number[] = [];
+
+  for (let step = 0; step < times.length; step++) {
+    let winCount = 0;
+    const time = times[step];
+    const record = records[step];
+
+    for (let i = 0; i < time; i++) {
+      const dist = i * (time - i);
+      const isWin = dist > record;
+
+      if (isWin) {
+        winCount++;
+      }
+    }
+
+    wins.push(winCount);
+  }
+
+  return multiply(wins);
+}
+
+run();
