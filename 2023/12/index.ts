0742b4f9c12b67f0644fdbe150693deba085ee0a
Grzegorz Klimek <kfiku.com@gmail.com>
2023-12-14 09:43:42 +0100

    lint

--- 2023/12/index.ts
+++ 2023/12/index.ts
@@ -1,5 +1,11 @@
 import { lineByLine, printResults, sum } from "../utils/utils.ts";
 
+// ??????????? 3,2,2 => l:12, m:9, miejsc do wstawienia: 4
+// (4 + 3 - 1)!
+// ------------------
+// 3! * ((4 + 3 - 1) - 3)!
+
+
 const part = +Deno.args[0] || 1;
 
 async function run() {
@@ -7,6 +13,7 @@ async function run() {
     await lineByLine("./input.txt", part1);
     const result = process1();
     printResults(8270, result);
+    printResults(21, result);
   } else {
     await lineByLine("./input.txt", part2);
     const result = process1();
@@ -18,39 +25,163 @@ interface Spring {
   list: string;
   nums: number[];
 }
+const lines: string[] = [];
 const springs: Spring[] = [];
+
 function part1(line: string) {
-  const [list, nums] = line.split(" ");
+  const { list, nums } = minimize(line);
 
   const spring = {
     list,
-    nums: nums.split(",").map((n) => +n),
+    nums,
   };
 
   springs.push(spring);
 }
 
 function part2(line: string) {
-  const [list, nums] = line.split(" ");
+  const { list, nums } = minimize(line);
 
   const spring = {
     list: [list, list, list, list, list].join("?"),
-    nums: nums.split(",").map((n) => +n).map((n) => [n, n, n, n, n]).flat(),
+    nums: nums.map((n) => [n, n, n, n, n]).flat(),
   };
 
   springs.push(spring);
+}
+
+function minimize(line: string) {
+  lines.push(line);
+  const [l, n] = line.split(" ");
+  const nn = n.split(",").map((n) => +n);
+
+  const { list: l1, nums: n1 } = minimizeOptions(l, [...nn]);
+  const { list, nums } = minimizeReverse(l1, [...n1]);
+
+  // if (nums.length !== nn.length) {
+  //   console.log('OPTIMIZE', l, nn, '=>', list, nums);
+  // } else {
+  //   console.log(':(', list, nums);
+  // }
+
+  return { list, nums }
+  return { list: l, nums: nn }
+}
+
+function minimizeOptions(l: string, nums: number[]) {
+  let list = trimList(l);
+  // console.log('l', list, nums);
+
+  const toCheck = nums[0]
+  const str = list.slice(0, toCheck + 2)
+
+  if (str.slice(toCheck) === '#.') {
+    // console.log('last # minimize', toCheck, list, '=>', list.slice(toCheck + 1));
+    nums.splice(0, 1)
+    list = list.slice(toCheck + 1)
+
+    return minimizeOptions(list, nums)
+  }
+
+  if (str[0] === '#') {
+    nums.splice(0, 1)
+    list = list.slice(toCheck + 1)
+
+    return minimizeOptions(list, nums)
+  }
+
+  // if (lParts[0].length === nums[0] + 1) {
+    //   nums.splice(0, 1)
+    //   list = list.slice(toCheck + 1)
+
+    //   return minimizeOptions(list, nums)
+    // }
+
+  // const lParts = list.split('#.')
+  // if (lParts.length > 1 && lParts.length === nums.length) {
+  //   const group = {
+  //     list: "",
+  //     nums: [] as number[]
+  //   }
+  //   for (let i = 0; i < lParts.length; i++) {
+  //     const { list: l1, nums: n1 } = minimizeOptions(lParts[i], [nums[i]]);
+  //     const { list: l2, nums: n2 } = minimizeReverse(l1, [...n1]);
+
+  //     group.list += l2
+  //     group.nums = [...group.nums, ...n2]
+  //   }
+
+  //   // console.log({group});
+  //   list = group.list
+  //   nums = group.nums
+  // }
+
+  return {
+    list,
+    nums
+  }
+}
+
+function minimizeReverse(l: string, n: number[]) {
+  const lr = stringReverse(l);
+  const nr = arrayReverse(n);
+
+  const { list, nums } = minimizeOptions(lr, nr);
+
+  return {
+    list: stringReverse(list),
+    nums: arrayReverse(nums)
+  }
+}
+
+function stringReverse(l: string) {
+  let rl = ""
+  const ll = l.length
+
+  for (let i = ll - 1; i >= 0; i--) {
+    rl += l[i]
+  }
+
+  return rl
+}
+
+function arrayReverse<T>(a: T[]) {
+  const ra: T[] = []
+  const l = a.length
+
+  for (let i = 0; i < l; i++) {
+    const element = a[i];
+    ra[l - i - 1] = element
+  }
+
+  return ra
 }
 
 function process1() {
   const counts: number[] = [];
 
   for (let i = 0; i < springs.length; i++) {
-    const element = springs[i];
+    if(i === 4) {
 
-    counts.push(countOptions(element));
+      console.log('STEP:', i, springs[i], lines[i]);
+    }
+    const opts = countOptions(springs[i]);
+
+    if(opts === 0) {
+      console.log('ERROR', springs[i], lines[i]);
+    }
+
+    counts.push(opts || 1);
   }
 
   return counts;
+}
+
+function trimList(list:string) {
+  return list
+    .replace(/\.+/g, '.')
+    .replace(/^\.+/g, '')
+    .replace(/\.+$/g, '')
 }
 
 function fill(
@@ -58,17 +189,20 @@ function fill(
   before: number[],
   len: number,
   fillWith: number,
+  validate: (fillArray: number[]) => boolean
 ) {
   if (len <= 1) {
     const nextBefore = [...before, fillWith];
-    arr.push(nextBefore);
+    if (validate(nextBefore)) {
+      arr.push(nextBefore);
+    }
   } else {
     const nexLen = len - 1;
 
     let l = fillWith;
     while (l >= 0) {
       const nextBefore = [...before, l];
-      fill(arr, nextBefore, nexLen, fillWith - l);
+      fill(arr, nextBefore, nexLen, fillWith - l, validate);
       l--;
     }
   }
@@ -80,7 +214,7 @@ function countOptions({ list, nums }: Spring) {
   const minLength = sum(nums) + between;
   const placesToInsert = between + 2;
   const dotsToPlay = fullLength - minLength;
-  const options = new Set();
+  // const options = new Set();
 
   if (fullLength === minLength) {
     return 1;
@@ -90,21 +224,19 @@ function countOptions({ list, nums }: Spring) {
     id ? "." + d : d
   );
 
-  const filles: number[][] = [];
-  fill(filles, [], placesToInsert, dotsToPlay);
-
-  for (let i = 0; i < filles.length; i++) {
-    const fill = filles[i].map((f) => getMultipleSigns(".", f));
+  const validate = (fillArray: number[]) => {
+    const fill = fillArray.map((f) => getMultipleSigns(".", f));
     const a = zip(fill, base);
 
     const p = a.join("");
 
-    if (validateOption(p, list)) {
-      options.add(p);
-    }
+    return validateOption(p, list);
   }
 
-  return options.size;
+  const filles: number[][] = [];
+  fill(filles, [], placesToInsert, dotsToPlay, validate);
+
+  return filles.length;
 }
 
 function validateOption(proposition: string, list: string) {
--- 2023/14/index.ts
+++ 2023/14/index.ts
@@ -6,24 +6,203 @@ async function run() {
   if (part === 1) {
     await lineByLine("./input.txt", part1);
     const result = process1();
-    printResults(405, result);
+    printResults(136, result);
   } else {
     await lineByLine("./input.txt", part1);
-    const result = process1();
+    const result = process2();
     printResults(400, result);
   }
 }
 
-const mirrors: string[][] = [[]];
+let lines: string[][] = [];
 
 function part1(line: string) {
-  console.log(line);
+  lines.push(line.split(''))
+}
+
+function process2() {
+  const full = 1_000_000_000
+  const onePercentile = full / 1000
+
+  for (let i = 0; i < 1_000_000_000; i++) {
+    if (i % onePercentile === 0) {
+      console.log((i / onePercentile) / 10 + "%");
+    }
+    spinCycle()
+  }
+
+  print()
+
+  return count();
+}
+
+function spinCycle() {
+  for (let x = 0; x < lines[0].length; x++) {
+    for (let y  = 0; y < lines.length; y++) {
+      const d = lines[y][x];
+      if (d === 'O') {
+        moveNorth(lines, x, y)
+      }
+    }
+  }
+  // print()
+  // console.log('==========');
+
+  for (let x = 0; x < lines[0].length; x++) {
+    for (let y  = 0; y < lines.length; y++) {
+      const d = lines[y][x];
+      if (d === 'O') {
+        moveWest(lines, x, y)
+      }
+    }
+  }
+  // print()
+  // console.log('==========');
+
+  for (let x = 0; x < lines[0].length; x++) {
+    for (let y = lines.length - 1; y >= 0; y--) {
+      const d = lines[y][x];
+      if (d === 'O') {
+        moveSouth(lines, x, y)
+      }
+    }
+  }
+  // print()
+  // console.log('==========');
+
+  for (let x = lines[0].length -1; x >= 0; x--) {
+    for (let y  = 0; y < lines.length; y++) {
+      const d = lines[y][x];
+      if (d === 'O') {
+        moveEast(lines, x, y)
+      }
+    }
+  }
+  // print()
+  // console.log('==========');
 }
 
 function process1() {
-  return [0]
+  for (let x = 0; x < lines[0].length; x++) {
+    for (let y  = 0; y < lines.length; y++) {
+      const d = lines[y][x];
+      if (d === 'O') {
+        moveNorth(lines, x, y)
+      }
+    }
+  }
+
+  print();
+
+  return count();
 }
 
+function count() {
+  const counts: number[] = [];
+  for (let x = 0; x < lines[0].length; x++) {
+    for (let y = 0; y < lines.length; y++) {
+      const d = lines[y][x];
+      if (d === 'O') {
+        const score = lines.length - y;
+        counts.push(score);
+      }
+    }
+  }
+
+  return counts;
+}
+
+function print() {
+  console.log(lines.map(l => l.join("")).join('\n'));
+}
+
+function moveNorth(lines: string[][], x: number, y: number) {
+  let ny = y;
+  let canGoNorth = y !== 0;
+  while(canGoNorth) {
+    const nextD = lines[ny-1]?.[x] || '#';
+    canGoNorth = ny > 0 && nextD === '.'
+    if (canGoNorth) {
+      ny--
+    }
+  }
+
+  if (ny !== y) {
+    lines[ny][x] = "O"
+    lines[y][x] = "."
+  }
+}
+
+function moveWest(lines: string[][], x: number, y: number) {
+  let nx = x;
+  let canGoWest = x !== 0;
+
+  while(canGoWest) {
+    const nextD = lines[y]?.[nx-1] || '#';
+    canGoWest = nx > 0 && nextD === '.'
+    if (canGoWest) {
+      nx--
+    }
+  }
+
+  if (nx !== x) {
+    lines[y][nx] = "O"
+    lines[y][x] = "."
+  }
+}
+
+function moveSouth(lines: string[][], x: number, y: number) {
+  let ny = y;
+  let canGoNorth = y !== lines.length -1;
+  while(canGoNorth) {
+    const nextD = lines[ny+1]?.[x] || '#';
+    canGoNorth = ny < lines.length -1 && nextD === '.'
+    if (canGoNorth) {
+      ny++
+    }
+  }
+
+  if (ny !== y) {
+    lines[ny][x] = "O"
+    lines[y][x] = "."
+  }
+}
+
+function moveEast(lines: string[][], x: number, y: number) {
+  let nx = x;
+  let canGoWest = x !== lines[0].length -1;
+
+  while(canGoWest) {
+    const nextD = lines[y]?.[nx+1] || '#';
+    canGoWest = nx <  lines[0].length -1 && nextD === '.'
+    if (canGoWest) {
+      nx++
+    }
+  }
+
+  if (nx !== x) {
+    lines[y][nx] = "O"
+    lines[y][x] = "."
+  }
+}
+
+function arrayRotate(arr: string[][]) {
+  const newArr: string[][] = [];
+
+  for (let y = 0; y < arr.length; y++) {
+    const a = arr[y];
+    for (let x = 0; x < a.length; x++) {
+      const nx = a.length - x - 1
+      if (!newArr[nx]) {
+        newArr[nx] = Array.from(new Array(arr.length));
+      }
+      const b = a[x];
+      newArr[nx][y] = b;
+    }
+  }
+
+  return newArr;
+}
 
 
 run();
