ba3cc6a5d9268e5d4e9c958ac03ad0186d62759e
Grzegorz Klimek <kfiku.com@gmail.com>
2024-12-09 08:06:34 +0100

    add test time

--- utils/run.ts
+++ utils/run.ts
@@ -12,18 +12,23 @@ export async function run(runFirstPart, runSecondPart, answers: Answers) {
     const inputLines = readFile('./input.txt')
     const test1Lines = readFile('./test1.txt')
     const test2Lines = readFile('./test2.txt')
+    let startTime = 0
 
+    startTime = performance.now()
     const test1result = runFirstPart(test1Lines, answers)
-    printResult('test1', test1result, answers)
+    printResult('test1', test1result, answers, startTime)
 
+    startTime = performance.now()
     const input1result = runFirstPart(inputLines, answers)
-    printResult('input1', input1result, answers)
+    printResult('input1', input1result, answers, startTime)
 
+    startTime = performance.now()
     const test2result = runSecondPart(test2Lines, answers)
-    printResult('test2', test2result, answers)
+    printResult('test2', test2result, answers, startTime)
 
+    startTime = performance.now()
     const input2result = runSecondPart(inputLines, answers)
-    printResult('input2', input2result, answers)
+    printResult('input2', input2result, answers, startTime)
   } catch (error) {
     if (error.message !== 'FAIL') {
       console.log(error)
@@ -31,12 +36,12 @@ export async function run(runFirstPart, runSecondPart, answers: Answers) {
   }
 }
 
-function printResult(key: keyof Answers, value: number, answers: Answers) {
+function printResult(key: keyof Answers, value: number, answers: Answers, startTime: number) {
   const expected = answers[key]
   const ok = value === expected
 
   if (ok) {
-    console.log(key, 'OK', value)
+    console.log(key, 'OK', value, ((performance.now() - startTime) / 1000).toFixed(6) + 's')
   } else {
     console.log(key, 'FAIL', value, 'should be:', expected)
 
