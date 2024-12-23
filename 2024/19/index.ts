a3de345cbc3528cbfa0218b2f356f9c7d3035f11
Grzegorz Klimek <kfiku.com@gmail.com>
2024-12-23 21:08:50 +0100

    day 19.1

--- 2024/19/index.ts
+++ 2024/19/index.ts
@@ -1,11 +1,9 @@
-import { byLine } from '../../utils/files'
-import { sum } from '../../utils/numbers'
 import { type Answers, InputType, run } from '../../utils/run'
 
 const answers: Answers = {
   test1: 6,
-  input1: 0,
-  test2: 0,
+  input1: 317,
+  test2: 16,
   input2: 0,
 }
 
@@ -16,30 +14,77 @@ function runFirstPart(input: string, type: InputType) {
   const patterns = p.split(', ')
   const designs = d.split('\n')
 
-  const availableDesigns = designs.map((d) => isPatternPossible('', d, patterns)).filter(Boolean)
+  const availableDesigns = designs.filter((design) => {
+    const designPatterns = patterns.filter((pattern) => design.includes(pattern))
+
+    return isPatternPossible('', design, designPatterns) === true
+  })
 
   return availableDesigns.length
 }
 
-function isPatternPossible(current: string, design: string, patterns: string[]) {
+function runSecondPart(input: string, type: InputType) {
+  const [p, d] = input.split('\n\n')
+  const patterns = p.split(', ')
+  const designs = d.split('\n')
+
+  const availableDesigns = designs.flatMap((design, i) => {
+    const designPatterns = patterns.filter((pattern) => design.includes(pattern))
+
+    console.log(i + 1)
+
+    return getPossiblePattern('', design, designPatterns)
+  })
+
+  return availableDesigns.length
+}
+
+function isPatternPossible(current: string, design: string, patterns: string[], cache = new Map<string, boolean>()) {
   const designLeft = design.substring(current.length, design.length)
+  const key = current + '_' + design
 
-  const patternsAvailable = patterns.filter((p) => designLeft.startsWith(p))
-  const patternsMatched = patterns.some((p) => design === current + p)
+  if (cache.has(key)) {
+    return false
+  }
 
+  cache.set(key, true)
+
+  const patternsMatched = patterns.some((pattern) => design === current + pattern)
   if (patternsMatched) {
     return true
   }
+
+  const patternsAvailable = patterns.filter((p) => designLeft.startsWith(p))
 
   if (patternsAvailable.length === 0) {
     return false
   }
 
-  return patternsAvailable.flatMap((p) => isPatternPossible(current + p, design, patterns)).some(Boolean)
+  return patternsAvailable
+    .flatMap((pattern) => isPatternPossible(current + pattern, design, patterns, cache))
+    .some(Boolean)
 }
 
-function runSecondPart(input: string, type: InputType) {
-  const lints = byLine(input)
+function getPossiblePattern(current: string, design: string, patterns: string[], cache = new Map<string, boolean>()) {
+  const designLeft = design.substring(current.length, design.length)
+  const key = current + '_' + design
 
-  return sum([lints.length])
+  if (cache.has(key)) {
+    return false
+  }
+
+  cache.set(key, true)
+
+  const patternsMatched = patterns.some((pattern) => design === current + pattern)
+  if (patternsMatched) {
+    return true
+  }
+
+  const patternsAvailable = patterns.filter((p) => designLeft.startsWith(p))
+
+  if (patternsAvailable.length === 0) {
+    return false
+  }
+
+  return patternsAvailable.flatMap((pattern) => getPossiblePattern(current + pattern, design, patterns))
 }
