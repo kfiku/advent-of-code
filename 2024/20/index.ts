abe838ca72e7ec34c48fab9993e9df2a4280b12d
Grzegorz Klimek <kfiku.com@gmail.com>
2024-12-26 12:49:28 +0100

    day 20.2

--- 2024/20/index.ts
+++ 2024/20/index.ts
@@ -1,14 +1,13 @@
-import { byLine, byLineAndLetter } from '../../utils/files'
+import { byLineAndLetter } from '../../utils/files'
 import { aStarSearch, Node } from '../../utils/matrix/aStar'
 import { getElementPosition, Matrix, Pos, posKey } from '../../utils/matrix/matrix'
-import { sum } from '../../utils/numbers'
 import { type Answers, InputType, run } from '../../utils/run'
 
 const answers: Answers = {
   test1: 1,
   input1: 1452,
-  test2: 0,
-  input2: 0,
+  test2: 7,
+  input2: 999556,
 }
 
 run(runFirstPart, runSecondPart, answers)
@@ -19,8 +18,8 @@ type NP = {
 }
 
 function runFirstPart(input: string, type: InputType) {
-  const minSaves = type === 'test' ? 60 : 100
   const { path, numberedPoints } = parseInput(input)
+  const minSaves = type === 'test' ? 60 : 100
 
   const hits: number[] = []
   path.forEach(([x, y]) => {
@@ -40,9 +39,29 @@ function runFirstPart(input: string, type: InputType) {
 }
 
 function runSecondPart(input: string, type: InputType) {
-  const lints = byLine(input)
+  const { path, numberedPoints } = parseInput(input)
+  const minSaves = type === 'test' ? 74 : 100
+  const maxDistance = 20
 
-  return sum([lints.length])
+  const hits: number[] = []
+  path.forEach(([x, y]) => {
+    const position = numberedPoints.get(posKey([x, y]))!.position
+
+    path.forEach(([px, py]) => {
+      const distance = Math.abs(x - px) + Math.abs(y - py)
+
+      if (distance <= maxDistance) {
+        const nextPosition = numberedPoints.get(posKey([px, py]))!.position
+        const save = nextPosition - position - distance
+
+        if (save >= minSaves) {
+          hits.push(save)
+        }
+      }
+    })
+  })
+
+  return hits.length
 }
 
 function parseInput(input: string) {
